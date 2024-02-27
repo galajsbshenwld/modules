481da71233341044fe9406a6f9d7040087694a26
galajsbshenwld <161507034+galajsbshenwld@users.noreply.github.com>
2024-02-27 20:31:21 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import mymodule
-import othermodule
-
-s = "Hello, world!"
-print(f"Reversed using mymodule: {mymodule.reverse(s)}")
-print(f"Reversed using othermodule: {othermodule.reverse(s)}")
-
